8bd63df4ef386da2b355c6650c337d8e565cefe5
Barry Mattern <bdmattern@gmail.com>
2022-09-12 11:18:05 -0600

    Update main.py & add index.html

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.linkedin.com/jobs/'
-page = requests.get(URL)
+url = 'https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-'
 
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup)
+response = requests.get(url)
+doc = BeautifulSoup(response.text, 'html.parser')
+
+prices = doc.find_all(text='$')
+parent = prices[0].parent
+strong = parent.find('strong')
+
+print(strong.string)
