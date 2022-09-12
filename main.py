efcc42d904460a50265b94dc4acd2edcf0529fe0
Barry Mattern <bdmattern@gmail.com>
2022-09-12 12:02:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from bs4 import BeautifulSoup
-import re
+import requests
 
-with open('index.html', 'r') as f:
-    doc = BeautifulSoup(f, 'html.parser')
+url = 'https://coinmarketcap.com/'
+result = requests.get(url).text
+doc = BeautifulSoup(result, 'html.parser')
 
-tags = doc.find_all('input', type='text')
-for tag in tags:
-    tag['placeholder'] = 'I changed you!'
+tbody = doc.tbody
+trs = tbody.contents
 
-with open('changed.html', 'w') as file:
-    file.write(str(doc))
+prices = {}
+
+for tr in trs[:10]:
+    name, price = tr.contents[2:4]
+    fixed_name = name.p.string
+    fixed_price = price.a.string
+
+    prices[fixed_name] = fixed_price
+
+print(prices)
