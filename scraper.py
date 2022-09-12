e50355da10abe6b65d2bd4c0ad4073abd3200fa0
Barry Mattern <bdmattern@gmail.com>
2022-09-12 15:21:48 -0600

    * renamed:    main.py -> bs4-tutorial.py * deleted:    changed.html * deleted:    index.html * new file:   linked-in-url.txt * new file:   scraper.py

---
+++ scraper.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+keywords = input('Enter job title: ').replace(' ', '%20').lower()
+url = f'https://www.linkedin.com/jobs/search?keywords={keywords}&location=&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
+
+response = requests.get(url).text
+
+print(response)
