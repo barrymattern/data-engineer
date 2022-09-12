06ba3e0c7d1325f9e3f3239081fd3459b3e52f91
Barry Mattern <bdmattern@gmail.com>
2022-09-12 15:49:02 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,5 +5,8 @@ keywords = input('Enter job title: ').replace(' ', '%20').lower()
 url = f'https://www.linkedin.com/jobs/search?keywords={keywords}&location=&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
 
 response = requests.get(url).text
+doc = BeautifulSoup(response, 'html.parser')
 
-print(response)
+job_list = doc.find(class_='jobs-search__results-list')
+
+print(len(job_list))
