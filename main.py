f71b394d1657b4c1c959b1cbd3771aeb99b39d52
Barry Mattern <bdmattern@gmail.com>
2022-09-12 14:12:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-url = 'https://coinmarketcap.com/'
-result = requests.get(url).text
-doc = BeautifulSoup(result, 'html.parser')
+search_term = input('What product do you want to search for? ')
 
-tbody = doc.tbody
-trs = tbody.contents
+url = f'https://www.newegg.com/p/pl?d={search_term}'
+page = requests.get(url).text
+doc = BeautifulSoup(page, 'html.parser')
 
-prices = {}
+page_text = doc.find(class_='list-tool-pagination-text').strong
+pages = int(str(page_text).split('/')[-2].split('>')[-1][:-1])
 
-for tr in trs[:10]:
-    name, price = tr.contents[2:4]
-    fixed_name = name.p.string
-    fixed_price = price.a.string
+items_found = {}
 
-    prices[fixed_name] = fixed_price
+for page in range(1, pages + 1):
+    url = f'https://www.newegg.com/p/pl?d={search_term}&page={page}'
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, 'html.parser')
 
-print(prices)
+    div = doc.find(
+        class_='item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell')
+    items = div.find_all(text=re.compile(search_term))
+
+    for item in items:
+        parent = item.parent
+        if parent.name != 'a':
+            continue
+
+        link = parent['href']
+        next_parent = item.find_parent(class_='item-container')
+        try:
+            price = next_parent.find(class_='price-current').find('strong').string
+            items_found[item] = {'price': int(price.replace(',', '')), 'link': link}
+        except:
+            pass
+
+sorted_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
+
+for item in sorted_items:
+    price = item[1]['price']
+    print(item[0])
+    print(f'${price}')
+    print(item[1]['link'])
+    print('-------------------------------------------------------------------')
